856dde180779400c1d77208072e1583c66537d0e
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2016-03-15 10:43:01 -0700

    free error detection

--- pa3/myfree.c
+++ pa3/myfree.c
@@ -6,5 +6,21 @@
 #include"mymalloc.c"
 
 void myfree(void* ptr, char* errorLocation, int errorLine) {
+	ptr = (mementryPtr) ptr;
+	if (strcmp(ptr->confirmCode, "dEL8zWd9Ik") != 0) {
+		fprintf(stderr, "Invalid pointer. Aborting...\n(Error at %s, line%d)\n", errorLocation, errorLine);
+		return;
+	}
+
+	if (ptr > 5000) {
+		fprintf(stderr, "Invalid pointer. Aborting...\n(Error at %s, line%d)\n", errorLocation, errorLine);
+		return;
+	}
+
+	if (ptr->isFree) {
+		fprintf(stderr, "Double Free\n(Error at %s, line%d)\n", errorLocation, errorLine);
+		return;
+	}
+
 	
 }

ea92914ec0fb35814d7db5e28c8d8f64d976b440
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2016-03-15 10:55:57 -0700

    pseudocode for most free opperations

--- pa3/myfree.c
+++ pa3/myfree.c
@@ -7,20 +7,37 @@
 
 void myfree(void* ptr, char* errorLocation, int errorLine) {
 	ptr = (mementryPtr) ptr;
-	if (strcmp(ptr->confirmCode, "dEL8zWd9Ik") != 0) {
-		fprintf(stderr, "Invalid pointer. Aborting...\n(Error at %s, line%d)\n", errorLocation, errorLine);
-		return;
-	}
 
+	//Could this be a valid pointer
 	if (ptr > 5000) {
 		fprintf(stderr, "Invalid pointer. Aborting...\n(Error at %s, line%d)\n", errorLocation, errorLine);
 		return;
 	}
 
+	//Is it a valid initialized ptr?
+	if (strcmp(ptr->confirmCode, "dEL8zWd9Ik") != 0) {
+		fprintf(stderr, "Invalid pointer. Aborting...\n(Error at %s, line%d)\n", errorLocation, errorLine);
+		return;
+	}
+
+	//Has it already been freed?
 	if (ptr->isFree) {
 		fprintf(stderr, "Double Free\n(Error at %s, line%d)\n", errorLocation, errorLine);
 		return;
 	}
 
-	
+	//free it
+	ptr->isFree = 1;
+
+	//is the one after it free?
+	if (ptr -> next != NULL && ptr -> next -> isFree) {
+		ptr -> sizeOfAllocation += ptr -> next -> sizeOfAllocation + sizeof(mementry);
+		ptr -> next = ptr -> next -> next;
+	}
+
+	//Is the one before it free?
+	if (ptr -> prev != NULL && ptr -> prev -> isFree) {
+		ptr -> prev -> sizeOfAllocation += ptr -> sizeOfAllocation + sizeof(mementry);
+		ptr -> prev -> next = ptr -> next;
+	}
 }

6d300dd8b32269dca78da27ad54f8803e53f332c
Parth-Mehrotra <parth.mehrotra.cs@gmail.com>
2016-04-11 16:38:00 -0400

    edtie

--- pa4/file_stats.c
+++ pa4/file_stats.c
@@ -1,34 +1,80 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <limits.h>
 
 #include "header.h"
 
+hashTablePtr table;
 
-void recursively_hash(DIR* dir, hashTablePtr table) {
-	if (dir -> d_type == DT_REG) {
-		hashFile(table, dir->d_name);
-		return;
+void find_files(char* dir_name) {
+	DIR* d;
+	d = opendir(dir_name);
+
+	if (d == NULL) {
+		hashFile(table, dir_name);
 	}
-	
-	while (1) {
-		if (dir != NULL) {
-			recursively_hash(readdir(dir));
-		} else {
-			return;
+
+	while (1) { 
+		struct dirent* entry; 
+		char* d_name;
+
+		entry = readdir(d);
+		if (entry == NULL) {
+			break;
+		}
+
+		d_name = entry -> d_name;
+		if (entry -> d_type == DT_REG) { 
+			hashFile(table, d_name);
+		}
+
+		if (entry -> d_type == DT_DIR) {
+			if (strcmp (d_name, "..") != 0 && strcmp (d_name, ".") != 0) {
+				int path_length;
+				char path[PATH_MAX];
+				path_length = snprintf (path, PATH_MAX, "%s/%s", dir_name, d_name);
+				find_files(path);
+			}
 		}
 	}
+
+	closedir(d);
 }
 
 int main(int argc, char** argv) {
-	DIR* dir = opendir(argv[2]);
-	hashTablePtr table = createTable(1000);
-	if (dir != NULL) {
-		recursively_hash(dir, table);
-	} else {
-		hashFile(table, dir->d_name);
+	table = createTable(1000);
+	find_files(argv[2]);
+	int length = table -> numEntries;
+	char** words = getSortedList(table);
+
+	FILE *fp;
+	fp = fopen(argv[1], "w+");
+
+	fprintf(fp, "{\"list\":[\n");
+	int i;
+	for (i = 0; i < length; i++) {
+		NodePtr node = getListOfFiles(words[i]);
+		fprintf(fp, "\"%s\":[\n", words[i]);
+
+		NodePtr temp = node;
+		while (1) {
+			fprintf(fp, "{\"%s\":%d}", parseFilePath(temp -> filePath), temp -> frequency);
+			if (temp -> next == NULL) {
+				fprintf(fp, "\n");
+				break;
+			} else {
+				fprintf(fp, ",\n");
+			}
+		}
+
+		if (i < length - 1) {
+			fprintf(fp, "]},\n");
+		} else {
+			fprintf(fp, "]}\n");
+		}
 	}
 
-	getSortedList(table);
+	fprintf(fp, "]}\n");
 
 	return 0;
 }
